15734f995578e46349f45eb4c643a8f8aac421d3
jschieke <jschieke@asu.edu>
2019-02-18 14:55:07 -0700

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -16,13 +16,20 @@ const constants = {
 function checkAuth() {
     // No authentification
     if (localstorage.auth == 'undefined') {
-        document.getElementById(logOutButton).style.display
+        document.getElementById(logOutButton).style.display = "none";
+        document.getElementById(logInButton).style.display = "block";
+        document.getElementById(addItemButton).style.display = "none";
+        document.getElementById(createAccountButton).style.display = "block";
         // No not display log out or add items buttons
         // log out button has id of logOutButton
         // log in button has id of logInButton
         // add items has id of addItemButton
         // create account button has id of createAccountButton
     } else {
+        document.getElementById(logOutButton).style.display = "block";
+        document.getElementById(logInButton).style.display = "none";
+        document.getElementById(addItemButton).style.display = "block";
+        document.getElementById(createAccountButton).style.display = "none";
         // Display log out and add items buttons, hide log in button
     }
 }
