170cf3b1d0c797847cbb36d12867b46dc19b559f
jschieke <jschieke@asu.edu>
2019-02-26 11:03:09 -0700

    Revert "Resolving conflicts"
    This reverts commit 48fc31637226899e91e501d78ae8936fbc5a15a5, reversing
    changes made to 60fe1da3faeb3e8ae82df13d826fa8ae3d8ac658.

--- index.js
+++ index.js
@@ -103,7 +103,6 @@ app.get('/api/verify-user', function(req, res) {
     con.query(queryString, function (err, result, fields) {
 
         if (err) throw err;
-        console.log(result)
         res.send(result)
       });
 });
